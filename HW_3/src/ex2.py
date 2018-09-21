5af943bc21b90c26ca0a4f27b439c5ff73ee75bb
pLOPeGG <douzont@gmail.com>
2018-09-21 22:16:49 +0200

    TD: ex2 finished

--- HW_3/src/ex2.py
+++ HW_3/src/ex2.py
@@ -1,3 +1,4 @@
+from matplotlib import image as img
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -10,9 +11,26 @@ def plot_identity():
     plt.draw()
     plt.show()
 
-def main():
-    plot_identity()
+def plot_equal_error():
+    image = img.imread('HW_3/img/ex2d.png')
+    plt.imshow(image)
+    
+    points = [[50.3264, 30.5042], [356.812, 325.483]]
+    plt.plot(*zip(*points))
+    inter = [[121.456], [99.5418]]
+    plt.scatter(*inter, marker='s')
+    plt.legend(['Antidiagonal', 'Intersection point'])
+    pass
+    plt.show()
 
+    roc_point = [(i[0]-b)/(e-b) for i, (b,e) in zip(inter, list(zip(*points)))]
+    roc_point[1] = 1 - roc_point[1]
+    print(*roc_point)
+
+
+def main():
+    # plot_identity()
+    plot_equal_error()
 
     pass
 
